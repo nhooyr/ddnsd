6c4ea22bc1e93e5bd056846098322362f6272cb0
aubble <anmol@aubble.com>
2015-07-19 00:52:42 -0400

    lots of refactoring and bug fixes

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # goDDNS
-DDNS client written in go!! working on adding more support for protocols, only supports namecheap atm
+DDNS client written in go!! working on adding more support for protocols, only supports namecheap atm. Look at wiki for config file
--- main.go
+++ main.go
@@ -2,151 +2,17 @@ package main
 
 import (
 	"encoding/json"
-	"errors"
-	"fmt"
+	"github.com/miekg/dns"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"regexp"
 	"strings"
 	"time"
-	"github.com/miekg/dns"
-	"regexp"
 )
 
 //TODO MORE PROTOCOLS
-
-type domain struct {
-	Host, Domain, Password, Protocol, ip, fqdn string
-	getIP                                      chan string
-}
-
-var validIP = regexp.MustCompile(`\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b`)
-
-func (c *domain) listenIPLoop() {
-	if c.Host == "@" {
-		c.fqdn = c.Domain + "."
-	} else {
-		c.fqdn = c.Host + "." + c.Domain + "."
-	}
-	var dnsIP string
-	dmsg := new(dns.Msg)
-	dmsg.SetQuestion(c.fqdn, dns.TypeA)
-	for newIP := range c.getIP {
-		log.Println("checking ip for", c.fqdn)
-		in, err := dns.Exchange(dmsg, "dns1.registrar-servers.com:53")
-		if err != nil {
-			log.Println(err)
-			continue
-		}
-		if len(in.Answer) >= 1 {
-			dnsIP = validIP.FindString(in.Answer[0].String())
-			log.Printf("current ip for %s is %s", c.fqdn, dnsIP)
-		} else {
-			log.Printf("could not find A record for %s", c.fqdn)
-			continue
-		}
-		if newIP != dnsIP {
-			err := c.updateIP(newIP)
-			if err != nil {
-				log.Println(err)
-			}
-		} else {
-			log.Println(c.fqdn, "up to date")
-		}
-	}
-}
-
-func (c *domain) useProtocol() (r *http.Response, err error) {
-	switch strings.ToLower(c.Protocol) {
-	case "namecheap":
-		r, err = http.Get("https://dynamicdns.park-your-domain.com/update?host=" + c.Host + "&domain=" + c.Domain + "&password=" + c.Password + "&ip=" + c.ip)
-	}
-	return
-}
-
-func (c *domain) checkError(buf []byte, r *http.Response) (errStr string, bad bool) {
-	s := (string(buf))
-	if strings.Contains(strings.ToLower(s), "error") {
-		bad = true
-		i := strings.Index(s, "<ResponseString>")
-		if i != -1 {
-			j := strings.Index(s, "</ResponseString>")
-			s = s[i+len("<ResponseString>") : j]
-		} else {
-			i = strings.Index(s, "<p>")
-			j := strings.Index(s, "</p>")
-			s = s[i+len("<p>") : j]
-
-		}
-		errStr = fmt.Sprintf("status code %d; could not update %s; %s", r.StatusCode, c.fqdn, s)
-	}
-	return
-}
-
-func (c *domain) updateIP(newIP string) error {
-	log.Println("updating", c.fqdn)
-	r, err := c.useProtocol()
-	if err != nil {
-		return err
-	}
-	defer r.Body.Close()
-	buf, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		return err
-	}
-	if errStr, bad := c.checkError(buf, r); bad {
-		err := errors.New(errStr)
-		return err
-	}
-	log.Println("updated", c.fqdn)
-	return nil
-}
-
-func (config *configuration) parseConfig() {
-	log.Println("reading config.json")
-	f, err := os.Open("config.json")
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println("read config.json")
-	log.Println("parsing config.json")
-	d := json.NewDecoder(f)
-	err = d.Decode(config)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println("parsed config.json")
-}
-
-func (config *configuration)checkIPLoop() {
-	for ;; time.Sleep(time.Second * config.Interval) {
-		log.Println("getting public IP")
-		resp, err := http.Get("http://echoip.com")
-		if err != nil {
-			log.Println(err)
-			continue
-		}
-		defer resp.Body.Close()
-		buf, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			log.Println(err)
-			continue
-		}
-		newIP := string(buf)
-		log.Println("got", newIP)
-		log.Println("sending IP to goroutines")
-		for _, c := range config.List {
-			c.getIP <- newIP
-		}
-		log.Println("sent IP to goroutines")
-	}
-}
-
-type configuration struct {
-	List     []*domain
-	Interval time.Duration
-}
 
 func main() {
 	log.SetPrefix("goDDNS: ")
@@ -161,15 +27,137 @@ func main() {
 	config.checkIPLoop()
 }
 
-//example config
-//{
-//"List": [
-//{
-//"Host": "@",
-//"Domain": "aubble.com",
-//"Password": "PASSWORDHERE",
-//"Protocol": "namecheap"
-//}
-//],
-//"Interval": 20 //seconds
-//} //REMOVE ALL COMMENTS PLEASE
+type configuration struct {
+	List     []*domain
+	Interval time.Duration
+}
+
+func (c *configuration) parseConfig() {
+	log.Println("reading config.json")
+	f, err := os.Open("config.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("read config.json")
+	log.Println("parsing config.json")
+	d := json.NewDecoder(f)
+	err = d.Decode(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("parsed config.json")
+}
+
+func (c *configuration) checkIPLoop() {
+	for ;; time.Sleep(time.Second * c.Interval) {
+		newIP, err := c.getPublicIP()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		log.Println("got", newIP)
+		log.Println("sending IP to goroutines")
+		for _, c := range c.List {
+			c.getIP <- newIP
+		}
+		log.Println("sent IP to goroutines")
+	}
+}
+
+func (c *configuration) getPublicIP() (string, error) {
+	log.Println("getting public IP")
+	resp, err := http.Get("http://echoip.com")
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	buf, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(buf), nil
+}
+
+type domain struct {
+	Host, Domain, Password, Protocol, ip, fqdn, dnsIP string
+	getIP                                             chan string
+	dnsMsg                                            *dns.Msg
+}
+
+func (d *domain) listenIPLoop() {
+	if d.Host == "@" {
+		d.fqdn = d.Domain + "."
+	} else {
+		d.fqdn = d.Host + "." + d.Domain + "."
+	}
+	d.dnsMsg = new(dns.Msg)
+	d.dnsMsg.SetQuestion(d.fqdn, dns.TypeA)
+	for newIP := range d.getIP {
+		d.checkIP()
+		if newIP != d.dnsIP {
+			d.updateIP(newIP)
+		} else {
+			log.Println(d.fqdn, "up to date")
+		}
+	}
+}
+
+var validIP = regexp.MustCompile(`\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b`)
+
+func (d *domain) checkIP() {
+	log.Println("checking ip for", d.fqdn)
+	in, err := dns.Exchange(d.dnsMsg, "dns1.registrar-servers.com:53")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	if len(in.Answer) >= 1 {
+		d.dnsIP = validIP.FindString(in.Answer[0].String())
+		log.Printf("current ip for %s is %s", d.fqdn, d.dnsIP)
+	} else {
+		log.Println("could not find A record for", d.fqdn)
+	}
+}
+
+func (d *domain) updateIP(newIP string) {
+	log.Println("updating", d.fqdn)
+	r, err := d.useProtocol()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer r.Body.Close()
+	buf, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println(err)
+	}
+	if bad := d.checkError(string(buf), r); bad {
+		return
+	}
+	log.Println("updated", d.fqdn)
+}
+
+func (d *domain) useProtocol() (r *http.Response, err error) {
+	switch strings.ToLower(d.Protocol) {
+	case "namecheap":
+		r, err = http.Get("https://dynamicdns.park-your-domain.com/update?host=" + d.Host + "&domain=" + d.Domain + "&password=" + d.Password + "&ip=" + d.ip)
+	}
+	return
+}
+
+func (d *domain) checkError(s string, r *http.Response) (bad bool) {
+	if strings.Contains(strings.ToLower(s), "error") {
+		bad = true
+		i := strings.Index(s, "<ResponseString>")
+		if i != -1 {
+			j := strings.Index(s, "</ResponseString>")
+			s = s[i+len("<ResponseString>") : j]
+		} else {
+			i = strings.Index(s, "<p>")
+			j := strings.Index(s, "</p>")
+			s = s[i+len("<p>") : j]
+		}
+		log.Printf("status code %d; could not update %s; %s", r.StatusCode, d.fqdn, s)
+	}
+	return
+}
